5fdc044cd745d92a218ba636e48a1fdf782c71f1
Adrien <adrien.delpire@gmail.com>
2020-07-20 12:08:07 +0200

    ecriture

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+(() => {
+
+    // your code here
+    // let count = 0;
+    let run = document.getElementById('img');
+    let storage = 0;
+    var target = document.getElementById("target");
+    
+    run.addEventListener('click',()=>{
+        let count = localStorage.getItem('score');    
+        count++;
+        parseInt(localStorage.setItem('score', count));
+        target.innerHTML = localStorage.getItem('score');
+    })
+})();
